81d28e750269cd6bb9af9072e6cf4796314085ef
codecraftcontact <157082059+codecraftcontact@users.noreply.github.com>
2024-05-16 12:04:42 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Javascript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Pagamento Pendente',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Css Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Reembolsado',
+        status: 'Finalizado'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
